90baa17242f64ce9e2c4f68981b9917a68fc337d
Paul McDowell <paulmcdowell18@gmail.com>
2024-05-17 10:31:09 -0400

    untrack config file

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA0LXiNZOaB_10CtJOQ_aKsxZe08yWOF3s",
-  authDomain: "sticker-safari.firebaseapp.com",
-  projectId: "sticker-safari",
-  storageBucket: "sticker-safari.appspot.com",
-  messagingSenderId: "264575108984",
-  appId: "1:264575108984:web:a6d5c123348d59a4d4aa8d",
-  measurementId: "G-SJLEK8LXRD",
-}
